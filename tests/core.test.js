99f4fd7400b97d8303c4f578a5ed076b8fb746cf
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-09 22:14:31 +0300

    Positive and negative testing

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,5 +1,5 @@
 import { it, expect, describe } from 'vitest'
-import { getCoupons } from '../src/core'
+import { calculateDiscount, getCoupons } from '../src/core'
 
 describe('getCoupons', () => {
     it('should return an array of coupons', () => {
@@ -28,6 +28,29 @@ describe('getCoupons', () => {
             expect(coupon.discount).toBeGreaterThan(0);
             expect(coupon.discount).toBeLessThan(1);
         })
+    });
+})
+
+describe('calculateDiscount', () => {
+    it('should return discounted price if given valid code', () => {
+        expect(calculateDiscount(10, 'SAVE10')).toBe(9);
+        expect(calculateDiscount(10, 'SAVE20')).toBe(8);
+    });
+
+    it('should handle non numeric price', () => {
+        expect(calculateDiscount('10', 'SAVE10')).toMatch(/invalid/i)
+    });
+
+    it('should handle negatice price', () => {
+        expect(calculateDiscount(-10, 'SAVE10')).toMatch(/invalid/i)
+    });
+
+    it('should handle non-string discount code', () => {
+        expect(calculateDiscount(10, 10)).toMatch(/invalid/i)
+    });
+
+    it('should handle invalid discount code', () => {
+        expect(calculateDiscount(10, 'INVALID')).toBe(10);
     });
 })
 
